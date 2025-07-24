6f19be6f5ce99d957b47d33e6e8ca81bbbe3409b
YUVAN SHANKAR S <yuvanshankar.s2023ai-ds@sece.ac.in>
2025-07-24 22:15:58 +0530

    commited

--- backend/middleware/adminAuth.js
+++ backend/middleware/adminAuth.js
@@ -12,7 +12,7 @@ const authMiddleware = (req, res, next) => {
   try {
     const decoded = jwt.verify(token, process.env.JWT_SECRET);
     console.log("Decoded token on backend:", decoded); 
-    req.user = decoded; // Must contain user ID (e.g., { id: user._id })
+    req.user = decoded;
     next();
   } catch (err) {
     return res.status(401).json({ message: "Invalid token" });
